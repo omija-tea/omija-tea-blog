c7a13e7dbb49d39c7f6ab079b9b49fac3093318e
omija-tea <playjnj@khu.ac.kr>
2026-05-27 14:21:19 +0900

    :lipstick: Add close button to blog explorer

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -120,6 +120,27 @@ export default ((userOpts?: Partial<Options>) => {
           </svg>
         </button>
         <div id={id} class="explorer-content" aria-expanded={false} role="group">
+          <button
+            type="button"
+            class="explorer-toggle mobile-explorer-close"
+            data-mobile={true}
+            aria-label="닫기"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </button>
           <OverflowList class="explorer-ul" />
         </div>
         <template id="template-file">
