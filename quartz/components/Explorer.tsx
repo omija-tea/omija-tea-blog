38c5b6d831be650ec0bac26b1afaf4ba4391facd
omija-tea <playjnj@khu.ac.kr>
2026-05-27 14:33:21 +0900

    :lipstick: Update blog explorer toggle button

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -131,14 +131,14 @@ export default ((userOpts?: Partial<Options>) => {
               width="24"
               height="24"
               viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
+              class="lucide-menu"
             >
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
+              <line x1="4" x2="20" y1="12" y2="12" />
+              <line x1="4" x2="20" y1="6" y2="6" />
+              <line x1="4" x2="20" y1="18" y2="18" />
             </svg>
           </button>
           <OverflowList class="explorer-ul" />
